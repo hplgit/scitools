569c64b61c04c6a6e8dbe642d846da0d3a4335b5
hpl <hpl@localhost>
2007-01-18 17:26:37 +0000

    updates

--- packages/scitools/lib/scitools/easyviz/doc/make.sh
+++ packages/scitools/lib/scitools/easyviz/doc/make.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 # clean:
 rm -rf tmp_*
+
+# HTML:
+# first covert to PNG format:
+cd figs
+for i in *.eps; do echo "convert $i"; if [ ! -f $i.png ]; then convert $i png:$i.png; fi; done
+cd ..
+cp easyviz.do.txt tmp_easyviz.do.txt
+doconce2format HTML tmp_easyviz.do.txt
+subst.py '\.eps"' '.eps.png"' tmp_easyviz.html
+
+exit
 
 doconce2format LaTeX easyviz.do.txt
 mv easyviz.ctex tmp_easyviz.ctex
@@ -9,3 +20,4 @@ latex tmp_easyviz
 latex tmp_easyviz
 dvips -o tmp_easyviz.ps tmp_easyviz.dvi
 ps2pdf tmp_easyviz.ps
+
