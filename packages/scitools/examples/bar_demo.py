1efc9b232e19bdb8f777e160e1d2a72c91ab65fb
johannr <johannr@localhost>
2007-09-03 09:29:34 +0000

    Added an example on how to use the bar function in scitools/examples/bar_demo.py

---
+++ packages/scitools/examples/bar_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from scitools.all import *
+from numpy.random import rand
+
+bar(rand(4,3))
+
+figure()
+x = seq(-2.9,2.9,0.2)
+data = exp(-x*x)
+
+if backend == 'gnuplot':
+    bar(data,0.7)
+else:
+    bar(data)
+
+
+figure()
+g = get_backend()
+h = bar(rand(3,2))
+if backend == 'gnuplot':
+    g('set xtics rotate ("A" 1, "B" 2, "C" 3)')
+    g.replot()
+
+raw_input('press enter')
