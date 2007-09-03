d8dc8c8ed7500f4b26d58d46f1d6348db3bfd015
johannr <johannr@localhost>
2007-09-03 14:55:33 +0000

    Updated bar graph example in scitools/example

--- packages/scitools/examples/bar_demo.py
+++ packages/scitools/examples/bar_demo.py
@@ -3,23 +3,30 @@
 from scitools.all import *
 from numpy.random import rand
 
+# simple example:
 bar(rand(4,3))
 
+# example with a vector:
 figure()
-x = seq(-2.9,2.9,0.2)
-data = exp(-x*x)
-
+x = linspace(-3,3,15)
+y = exp(-x*x)
 if backend == 'gnuplot':
-    bar(data,0.7)
+    bar(y,0.7)  # increase width of bars
 else:
-    bar(data)
+    bar(y)
 
-
+# draw a bar graph from a dicitonary:
 figure()
-g = get_backend()
-h = bar(rand(3,2))
-if backend == 'gnuplot':
-    g('set xtics ("A" 1, "B" 2, "C" 3)')
-    g.replot()
+data = {'method1': {'thing1': 3, 'thing2': 2, 'thing3': 4},
+        'method2': {'thing1': 4, 'thing2': 4.5, 'thing3': 2},
+        'method3': {'thing1': 1, 'thing2': 4, 'thing3': 2},}
+bar(data,rotated_barticks=False,
+    hardcopy='bar1a.eps',color=True,
+    ymin=0,ymax=5)
+
+# border turned off:
+figure()
+bar(rand(5,4),shading='interp')
+
 
 raw_input('press enter')
