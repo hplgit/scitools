ae60d212280519d8ac3d0ff69a0b78d21e2d4cc4
johannr <johannr@localhost>
2007-09-06 07:06:28 +0000

    Added fill_demo.py to scitools/examples

---
+++ packages/scitools/examples/fill_demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Examples taken from fill_demo.py and fill_demo2.py in Matplotlib."""
+
+from scitools.all import *
+
+t = linspace(0.0, 1.0, 101)
+s = sin(2*2*pi*t)
+
+fill(t, s*exp(-5*t), 'r', grid=True)
+
+figure()
+t = linspace(0.0,3.0,301)
+s = sin(2*pi*t)
+c = sin(4*pi*t)
+fill(t, s, 'b', t, c, 'g', alpha=0.2)
+
+raw_input('press enter')
