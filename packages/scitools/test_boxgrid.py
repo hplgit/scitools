99a6862c52dc77b3cc452daa6d47f5183c973687
johannr <johannr@localhost>
2008-02-06 10:29:03 +0000

    updated test_boxgrid.py

--- packages/scitools/test_boxgrid.py
+++ packages/scitools/test_boxgrid.py
@@ -9,5 +9,28 @@ if 0:
 #u2 = d2.vectorized_eval(func)
 for i, j in g.iter('corners'):
     print g[i,j]
-for i, j in g.iter('all_boundary'): # Gives flat list instead of x,y pairs
-    print g[i,j]
+#for i, j in g.iter('all_boundary'): # Gives flat list instead of x,y pairs
+#    print g[i,j]
+
+
+#from scitools.all import *
+g = UniformBoxGrid(x=(0,2), nx=2, y=(0,2), ny=2)
+#values = g.coorv[0]*g.coorv[1]
+#surf(values)
+for i, j in g.iter('all'):
+    print 'all:', i, j
+    print 'all:', g.xcoor[i], g.ycoor[j]
+for i, j in g.iter('interior'):
+    print 'interior:', i, j
+    print 'interior:', g.xcoor[i], g.ycoor[j]
+for imin,imax, jmin,jmax in g.iter('all_boundary'):
+    print 'all_boundary:', imin,imax, jmin,jmax
+    print 'all_boundary:', \
+          g.xcoor[imin],g.xcoor[imax], g.ycoor[jmin],g.ycoor[jmax]
+for imin,imax, jmin,jmax in g.iter('interior_boundary'):
+    print 'interior_boundary:', imin,imax, jmin,jmax
+    print 'interior_boundary:', \
+          g.xcoor[imin],g.xcoor[imax], g.ycoor[jmin],g.ycoor[jmax]
+for i, j in g.iter('corners'):
+    print 'corners:', i,j
+    print 'corners:', g.xcoor[i], g.ycoor[j]
