8f17c9f5c161bfa53db714c751bb075f6d33fe1f
langtangen <langtangen@localhost>
2009-02-16 07:30:32 +0000

    removed Diffpack-specific statements in regression.py

--- bin/regression.py
+++ bin/regression.py
@@ -6,29 +6,18 @@ usage = "Usage: " + sys.argv[0] + " verify|update|template rootdir|filename"
 
 try:
   task = sys.argv[1]
-except:
+except IndexError:
   print usage; sys.exit(1)
 
 try:
-  root = sys.argv[2]
-except:
+  name = sys.argv[2]
+except IndexError:
   print usage; sys.exit(1)
 
-# options sys.argv[3] is only relevant for Diffpack applications:
-try:
-  mode = sys.argv[3]
-except:
-  mode = "opt"
-
-legal = ('verify', 'update', 'template')
 if task == 'verify' or task == 'update':
-    #v = scitools.Regression.Verify(root=root, task=task, makemode=mode)
-    # use VerifyDiffpack as this handles all simple .verify scripts
-    # and also scripts requiring compilation of Diffpack applications
-    v = scitools.Regression.VerifyDiffpack(root=root, task=task, makemode=mode)
-    # the constructor does it all...
+    v = scitools.Regression.Verify(root=name, task=task)
 elif task == 'template':
-    scitools.Regression.verify_file_template(root)  # sys.argv[2] is the name...
+    scitools.Regression.verify_file_template(name)
 else:
     print "1st command-line argument must be verify, update, or template"
     
