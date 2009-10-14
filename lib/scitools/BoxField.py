02918ceb20db95f3d0026452b200a86852bacabd
langtangen <langtangen@localhost>
2009-10-14 13:13:29 +0000

    Added support for DOLFIN-type mesh and scalar field transformation to BoxGrid/Field. Standard finite element fields can then be transformed to BoxFields and easily plotted with easyviz.

--- lib/scitools/BoxField.py
+++ lib/scitools/BoxField.py
@@ -4,7 +4,7 @@ Class for a scalar (or vector) field over a BoxGrid or UniformBoxGrid grid.
 """
 
 from scitools.BoxGrid import BoxGrid, UniformBoxGrid, X, Y, Z
-from numpy import zeros, array
+from numpy import zeros, array, transpose
 
 
 class Field(object):
@@ -46,8 +46,9 @@ class BoxField(Field):
         @param name: name of the field.
         @param vector: scalar field if 0, otherwise the no of vector
         components (spatial dimensions of vector field).
-        @param kwargs: optional keyword arguments, stored as metadata in
-        the field (see class Field)
+        @param kwargs:
+          values: optional array with field values
+          other arguments just stored as metadata in the field (see class Field)
         """
         Field.__init__(self, grid, name, **kwargs)
         
@@ -60,14 +61,16 @@ class BoxField(Field):
         else:
             required_shape = self.grid.shape
 
-        if 'values' in kwargs and values is not None: 
-            if values.shape == required_shape:
-                self.values = values  # field data are provided
-            else:
-                raise ValueError(
-                      'values array are incompatible with grid size; '\
-                      'shape is %s while required shape is %s' % \
-                      (values.shape, required_shape))
+        if 'values' in kwargs:
+            values = kwargs['values']
+            if values is not None: 
+                if values.shape == required_shape:
+                    self.values = values  # field data are provided
+                else:
+                    raise ValueError(
+                          'values array are incompatible with grid size; '\
+                          'shape is %s while required shape is %s' % \
+                          (values.shape, required_shape))
         else:
             # create array of scalar field grid point values:
             self.values = zeros(required_shape)
@@ -99,6 +102,11 @@ class BoxField(Field):
         Return a coordinate array and corresponding field values
         along a line starting with start_coor, in the given
         direction, and ending in end_coor (default: grid boundary).
+        Two more boolean values are also returned: fixed_coor
+        (the value of the fixed coordinates, which may be different
+        from those in start_coor if snap=True) and snapped (True if
+        the line really had to be snapped onto the grid, i.e.,
+        fixed_coor differs from coordinates in start_coor.
 
         If snap is True, the line is snapped onto the grid, otherwise
         values along the line must be interpolated (not yet implemented).
@@ -123,12 +131,14 @@ class BoxField(Field):
         >>> #plot(xc, uc, title='u(x, y=%g)' % fixed_coor)
         """
         if not snap:
-            raise Not ImplementedError('Use snap=True, no interpolation impl.')
+            raise NotImplementedError('Use snap=True, no interpolation impl.')
 
         slice_index, snapped = \
              self.grid.gridline_slice(start_coor, direction, end_coor)
         fixed_coor = [self.grid[s][i] for s,i in enumerate(slice_index) \
                       if not isinstance(i, slice)]
+        if len(fixed_coor) == 1:
+            fixed_coor = fixed_coor[0]  # avoid returning list of length 1
         return self.grid.coor[direction][slice_index[direction].start:\
                                          slice_index[direction].stop], \
                self.values[slice_index], fixed_coor, snapped
@@ -144,7 +154,7 @@ class BoxField(Field):
         Otherwise, the returned values must be interpolated (not yet impl.).
         """
         if not snap:
-            raise Not ImplementedError('Use snap=True, no interpolation impl.')
+            raise NotImplementedError('Use snap=True, no interpolation impl.')
 
         slice_index, snapped = self.grid.gridplane_slice(value, constant_coor)
         if constant_coor == 0:
@@ -158,8 +168,82 @@ class BoxField(Field):
             y = self.grid.coor[1]
         fixed_coor = self.grid.coor[constant_coor][slice_index[constant_coor]]
         return x, y, self.values[slice_index], fixed_coor, snapped
-    
 
+def _rank12rankd_mesh(a, shape):
+    """
+    Given rank 1 array a with values in a mesh with the no of points
+    described by shape, transform the array to the right "mesh array"
+    with the same shape.
+    """
+    shape = list(shape)
+    shape.reverse()
+    if len(a.shape) == 1:
+        return a.reshape(shape).transpose()
+    else:
+        # reshape first dimension, keep the others
+        total_shape = shape + list(a.shape[1:])
+        print 'total shape:', total_shape
+        a = a.reshape(total_shape)
+        return transpose(a, axes=None)
+    #return transpose(a, axes=range(len(shape)))
+
+def dolfin_mesh2UniformBoxGrid(dolfin_mesh, division):
+    """
+    Turn a regular, structured DOLFIN finite element mesh into
+    a UniformBoxGrid object. (Mostly for ease of plotting with scitools.)
+    Standard DOLFIN numbering numbers the nodes along the x[0] axis,
+    then x[1] axis, and so on.
+    """
+    c = dolfin_mesh.coordinates() # numpy array
+    return UniformBoxGrid(min=c[0], max=c[-1],
+                          division=division)
+    
+def dolfin_mesh2BoxGrid(dolfin_mesh, division):
+    """
+    Turn a structured DOLFIN finite element mesh into
+    a BoxGrid object. (Mostly for ease of plotting with scitools.)
+    Standard DOLFIN numbering numbers the nodes along the x[0] axis,
+    then x[1] axis, and so on.
+    """
+    c = dolfin_mesh.coordinates() # numpy array
+    shape = [n+1 for n in division]  # shape for points in each dir.
+    raise NotImplementedError # must redo how to reshape vector fields
+    c = _rank12rankd_mesh(c, shape)  # Ooops, might not work!!! works for scalars...
+    print 'reshaped coor:\n', c
+    print c[1,2]
+    if len(c.shape) == 1:
+        coor = [c]
+    elif len(c.shape) == 2:
+        coor = [c[:,0], c[0,:]]
+    elif len(c.shape) == 3:
+        coor = [c[:,0,0], c[0,:,0], c[0,0,:]]
+        
+    return BoxGrid(coor)
+
+
+def dolfin_function2BoxField(dolfin_function, dolfin_mesh,
+                             division, uniform_mesh=True):
+    """
+    Turn a DOLFIN finite element field over a structured mesh into
+    a BoxField object. (Mostly for ease of plotting with scitools.)
+    Standard DOLFIN numbering numbers the nodes along the x[0] axis,
+    then x[1] axis, and so on.
+    """
+    nodal_values = dolfin_function.vector().array().copy()
+    if len(nodal_values.shape) > 1:
+        raise NotImplementedError # no support for vector valued functions yet
+                                  # the problem is in _rank12rankd_mesh
+    if uniform_mesh:
+        grid = dolfin_mesh2UniformBoxGrid(dolfin_mesh, division)
+    else:
+        grid = dolfin_mesh2BoxGrid(dolfin_mesh, division)
+    try:
+        nodal_values = _rank12rankd_mesh(nodal_values, grid.shape)
+    except ValueError, e:
+        raise ValueError('DOLFIN function has vector of size %s while the provided mesh demands %s' % (nodal_values.size, grid.shape))
+    
+    return BoxField(grid, name=dolfin_function.name(), values=nodal_values)
+    
 def _test(g):
     print 'grid: %s' % g
 
