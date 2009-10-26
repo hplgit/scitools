1e1dfb005a3a78aad792f3db7bd703fe181d959e
langtangen <langtangen@localhost>
2009-10-26 17:25:54 +0000

    updates

--- lib/scitools/easyviz/doc/make.sh
+++ lib/scitools/easyviz/doc/make.sh
@@ -3,12 +3,17 @@
 rm -rf tmp_*
 
 cp easyviz.do.txt tmp_easyviz.do.txt
+
 doconce2format HTML tmp_easyviz.do.txt
 
 doconce2format LaTeX tmp_easyviz.do.txt
-ctex2tex -code2verb 1 tmp_easyviz
+ptex2tex tmp_easyviz
 latex tmp_easyviz
 latex tmp_easyviz
 dvips -o tmp_easyviz.ps tmp_easyviz.dvi
 ps2pdf tmp_easyviz.ps
+
+doconce2format wiki tmp_easyviz.do.txt
+
+
 
