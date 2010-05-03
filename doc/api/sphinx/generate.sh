3323aba14c4c50cc5230030c178c757ec6791161
langtangen <langtangen@localhost>
2010-05-03 05:34:30 +0000

    api doc works: generate.sh (but doc strings are not updated to reST yet)

--- doc/api/sphinx/generate.sh
+++ doc/api/sphinx/generate.sh
@@ -26,7 +26,7 @@ EOF
 
 scitools subst '\#sys\.path\.append.+' "sys.path.append(os.path.join(os.path.abspath('.'), os.pardir, os.pardir, os.pardir, 'lib', 'scitools'))" conf.py
 
-scitools subst ':maxdepth: 2' ":maxdepth: 2\n\n\n    BoxField\n    BoxGrid\n    EfficiencyTable\n    FloatComparison\n    Lumpy\n    NumPyDB\n    aplotter\n    basics\n    configdata\n    convergencerate\n    debug\n    easyviz\n    errorcheck\n    filetable\n    globaldata\n    modulecheck\n    multipleloop\n    numpytools\n    numpyutils\n    pprint2\n    PrmDictBase\n    Regression\n    sound\n    std\n    StringFunction" index.txt
+scitools subst ':maxdepth: 2' ":maxdepth: 2\n\n\n   BoxField\n   BoxGrid\n   EfficiencyTable\n   FloatComparison\n   Lumpy\n   NumPyDB\n   aplotter\n   basics\n   configdata\n   convergencerate\n   debug\n   easyviz\n   errorcheck\n   filetable\n   globaldata\n   modulecheck\n   multipleloop\n   numpytools\n   numpyutils\n   pprint2\n   PrmDictBase\n   Regression\n   sound\n   std\n   StringFunction" index.txt
 
 make html
 
