0b014f93094d1f814b041c56be333fd4695be9c6
Hans Petter Langtangen <hpl@simula.no>
2011-03-25 17:01:09 +0100

    added test example with text.

---
+++ examples/plot2n.py
@@ -0,0 +1,19 @@
+"""Test text in plot."""
+from scitools.std import *
+
+def f1(t):
+    return t**2*exp(-t**2)
+
+def f2(t):
+    return t**2*f1(t)
+
+t = linspace(0, 3, 51)
+y1 = f1(t)
+y2 = f2(t)
+
+plot(t, y1, 'r-', legend='t**2*exp(-t**2)',
+     title='Two exponential functions')
+text(1, 0.6, 'A text')
+show()
+savefig('tmp1.eps')
+raw_input('Press Return key to quit: ')
