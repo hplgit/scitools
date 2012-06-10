37db1ad84ec01d98f6cb2055db2aea0444fb291c
Johannes Ring <johannr@simula.no>
2012-06-10 17:55:28 +0200

    Add pyreport.

---
+++ lib/scitools/pyreport/version.py
@@ -0,0 +1,9 @@
+"""
+This file is required only for loose coupling and avoiding import loops.
+"""
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# Copyright: Gael Varoquaux
+# License: BSD
+
+__version__ = "0.3.4b"
+
